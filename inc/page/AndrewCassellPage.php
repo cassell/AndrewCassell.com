2ba3320068387155a8b3de5be1be790327bb577f
Andrew Cassell <alc277@gmail.com>
2013-12-19 09:44:06 -0500

    home page

--- inc/page/AndrewCassellPage.php
+++ inc/page/AndrewCassellPage.php
@@ -5,6 +5,8 @@ class AndrewCassellPage extends HtmlPage
 	function open()
 	{
 		$this->insertStyleSheet('/lib/vendor/frameworks/bootstrap/css/bootstrap.css');
+		$this->insertStyleSheet('/css/main.css');
+		$this->setHtmlTitle('Andrew Cassell - Web Application Developer - Herndon, Virginia');
 		$this->printHtmlHeader();
 	}
 	
