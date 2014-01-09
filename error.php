c579cbca58e55df5126b5bed4ec2270e40161db2
Andrew Cassell <alc277@gmail.com>
2014-01-09 09:12:16 -0500

    error page

--- error.php
+++ error.php
@@ -7,26 +7,7 @@ $page->setHtmlTitle("Error");
 $page->open();
 
 ?>
-<style>
-#me-photo img
-{
-	display: inline-block;
-	width: 160px;
-	height: 160px;
-	 border-radius: 100%;
-	-o-border-radius: 100%;
-	-webkit-border-radius: 100%;
-	-moz-border-radius: 100%;
-	opacity: .95;
-	border: 1px solid #5B6E7A;
-}
-</style>
-
-<div id="me-photo">
-		<img alt="Andrew Cassell" src="/img/andrew-cassell.jpg">
-</div>
-
-
+<h1>Error</h1>
 <?php
 
 
