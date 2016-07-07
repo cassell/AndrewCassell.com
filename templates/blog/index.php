51cea551b71cff82dd293e271cb0e3d18565375a
Andrew Cassell <alc277@gmail.com>
2016-07-06 23:30:28 -0400

    link to speaker deck

--- templates/blog/index.php
+++ templates/blog/index.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="blog-container">
         <h1>Blog Articles, Talks, and Presentations</h1>
+        <p>All Talks: <a href="https://speakerdeck.com/cassell">Speaker Deck</a></p>
         <?php foreach($entries as $year => $dates): ?>
            <h2><?= (int)$year ?></h2>
             <ul>
